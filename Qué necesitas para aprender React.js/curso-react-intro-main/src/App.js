084efb72f20a53606ec94ba8d22dc2cb721ace10
luisftroncoso2010 <troncober@hotmail.com>
2024-05-06 15:56:21 -0500

    Completando y eliminado TODOS

--- Qué necesitas para aprender React.js/curso-react-intro-main/src/App.js
+++ Qué necesitas para aprender React.js/curso-react-intro-main/src/App.js
@@ -16,7 +16,8 @@ const defaulTodos = [
 
 
 function App() {
-  const [todos] = React
+  // Hoks
+  const [todos, setTodos] = React
     .useState(defaulTodos);
   const [searchValue, setSearchValue] = React
     .useState('');  // Se cola un string vacio
@@ -34,8 +35,24 @@ function App() {
     } 
   )
 
-  // Mostramos el cambio de estado
-  console.log("Los usuarios buscan todos de " + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    )
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos)
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    )
+    newTodos.splice(todoIndex, 1)
+    setTodos(newTodos);
+  }
+
   return (
     <>   
       <TodoCounter
@@ -52,6 +69,8 @@ function App() {
          key={todo.text}
          text={todo.text}
          completed={todo.completed}
+         onComplete={() => completeTodo(todo.text)}
+         onDelete={() => deleteTodo(todo.text)}
          />
       ))};
       </TodoList> 
