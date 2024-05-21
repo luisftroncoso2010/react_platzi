15b9c18d5a2f0a658c568704df6460fad311382d
luisftroncoso2010 <troncober@hotmail.com>
2024-05-20 19:03:30 -0500

    Proyecto, Variar colores RGB

---
+++ javaScript para manipulacion del DOM/color-rgb/app.js
@@ -0,0 +1,46 @@
+const inputRojo = document.getElementById('rojo')
+const inputVerde = document.getElementById('verde')
+const inputAzul = document.getElementById('azul')
+
+
+const textoRojo = document.getElementById('texto-rojo')
+const textoVerde = document.getElementById('texto-verde')
+const textoAzul = document.getElementById('texto-azul')
+
+
+let rojo = inputRojo.value
+let verde = inputVerde.value
+let azul = inputAzul.value
+
+textoRojo.innerText = rojo
+textoVerde.innerText = verde
+textoAzul.innerText = azul
+
+function actualizarColor(rojo, verde, azul){
+    const colorRGB = `rgb(${rojo}, ${verde}, ${azul})`
+    document.body.style.backgroundColor = colorRGB
+}
+
+// Para actulizar el RED (Rojo)
+inputRojo.addEventListener('change', (e) => {
+    rojo = e.target.value
+    textoRojo.innerText = rojo
+    actualizarColor(rojo, verde, azul)
+})
+
+// Para actulizar el Green (Verde)
+inputVerde.addEventListener('change', (e) => {
+    verde = e.target.value
+    textoVerde.innerText = verde
+    actualizarColor(rojo, verde, azul)
+})
+
+// Para actulizar el Green (Verde)
+inputAzul.addEventListener('change', (e) => {
+    azul = e.target.value
+    textoAzul.innerText = azul
+    actualizarColor(rojo, verde, azul)
+})
+
+/* NOTA: Se deben coger atraves rgb, es decir
+desde la variable global */
